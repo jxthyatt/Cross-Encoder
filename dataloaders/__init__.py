9df4b2cd00eb535f57e3eb4676e82b0d4a9303e4
jxthyatt <95753326+jxthyatt@users.noreply.github.com>
2025-03-16 14:40:22 +0800

    Add files via upload

---
+++ dataloaders/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from .data_loader_Columbia import ImagerLoader as Columbia
+from .data_loader_UTMultiview import ImagerLoader as UTMultiview
+from .data_loader_MPIIGaze import ImagerLoader as MPIIGaze
+from .data_loader_XGaze import ImagerLoader as XGaze
+from .data_loader_unite import ImagerLoader as Unite
+from .data_loader_FreeGaze import ImagerLoader as FreeGaze
+
+__all__ = ('Columbia', 'UTMultiview', 'MPIIGaze', 'XGaze', 'Unite', 'FreeGaze')
